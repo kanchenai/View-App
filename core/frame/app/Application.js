18198537ce9ebb9b0be48c03b3d1a7b50262e8e9
mahangbin <2695704942@qq.com>
2022-07-21 00:06:11 +0800

    更新日期版本号

--- core/frame/app/Application.js
+++ core/frame/app/Application.js
@@ -11,7 +11,7 @@ require("../../css/style.css");
  * view-app的版本号
  * @type {string}
  */
-export var version = "0.2.0(2022-07-14)";
+export var version = "0.2.0(2022-07-20)";
 
 export default class Application extends GroupView {
     constructor(id) {
